d2faf9576aea0bec309dccb777911d03a1ebe7cf
SrGiovanni <samuelefulghi@gmail.com>
2022-03-09 11:45:29 -0500

    add passing Employee test and constructor

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,30 @@
+const { test, expect } = require('@jest/globals')
+const Employee = require('../lib/Employee')
+
+
+
+test('creates an employee object', () => {
+    const employee = new Employee('Jimmy', '1234', 'sampleMail@life.it')
+
+    expect(employee.name).toBe('Jimmy')
+    expect(employee.id).toBe('1234')
+    expect(employee.email).toBe('sampleMail@life.it')
+})
+
+test('checks employee name', () =>{
+    const employee = new Employee('Jimmy', '1234', 'sampleMail@life.it')
+
+    expect(employee.getName()).toBe('Jimmy')
+})
+
+test('check employee ID', () => {
+    const employee = new Employee('Jimmy', '1234', 'sampleMail@life.it')
+
+    expect(employee.getID()).toBe('1234')
+})
+
+test('check employee email', () => {
+    const employee = new Employee('Jimmy', '1234', 'sampleMail@life.it')
+
+    expect(employee.getEmail()).toBe('sampleMail@life.it')
+})
